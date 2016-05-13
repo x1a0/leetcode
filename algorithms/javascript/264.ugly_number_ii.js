2b29d3ac66016b61422ca9e73d7ed42d666ed6ab
x1a0 <zhangxiao@xzhang.net>
2016-05-13 15:50:51 +0200

    Better solution

--- algorithms/javascript/264.ugly_number_ii.js
+++ algorithms/javascript/264.ugly_number_ii.js
@@ -3,23 +3,16 @@
  * @return {number}
  */
 var nthUglyNumber = function(n) {
-  var buffer = [];
-  var i = 1;
-  var x = 1;
-  while (i < n) {
-    insert(x*2, buffer);
-    insert(x*3, buffer);
-    insert(x*5, buffer);
-    x = buffer.shift();
-    i++;
-  }
-  return x;
-}
+  var uglies = [1];
+  var p2 = 0, p3 = 0, p5 = 0;
 
-var insert = function(x, arr) {
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] === x) return;
-    if (arr[i] > x) break;
+  while (uglies.length < n) {
+    var next = Math.min(uglies[p2] * 2, uglies[p3] * 3, uglies[p5] * 5);
+    if (uglies[p2] * 2 === next) p2++;
+    if (uglies[p3] * 3 === next) p3++;
+    if (uglies[p5] * 5 === next) p5++;
+    uglies.push(next);
   }
-  arr.splice(i, 0, x);
+
+  return uglies[n - 1];
 }
